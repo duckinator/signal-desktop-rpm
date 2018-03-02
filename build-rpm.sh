95a4eb28bb1b2237b44c264d877b1316102e8457
Ellen Marie Dash <me@duckie.co>
2018-03-02 12:30:57 -0500

    *shrugs forever*

--- build-rpm.sh
+++ build-rpm.sh
@@ -10,9 +10,14 @@ function fail() {
   exit 1
 }
 
+if [ "$1" == "--clone" ]; then
 wget https://github.com/signalapp/Signal-Desktop/archive/v${SIGNAL_VERSION}.zip || fail "Failed to download zip archive."
 unzip v${SIGNAL_VERSION}.zip || fail "Failed to extract zip archive."
+fi
+
 pushd Signal-Desktop-${SIGNAL_VERSION} || fail "Signal-Desktop-${SIGNAL_VERSION} does not exist."
+
+if [ "$1" == "--clone" ]; then
 npm install -g yarn || fail "Failed to install yarn."
 npm install -g grunt-cli || fail "Failed to install grunt-cli."
 yarn install || fail "Error during 'yarn install'."
@@ -22,11 +27,17 @@ yarn test || fail "Test failure."
 echo '{"serverUrl": "https://textsecure-service.whispersystems.org", "cdnUrl": "https://cdn.signal.org"}' > local-development.json || fail "Failed to create local-development.json."
 yarn generate || fail "Error during 'yarn generate'."
 yarn build-release || fail "Error during 'yarn build-release'."
+fi
+
 pushd release || fail "release/ does not exist."
+sudo rm -rf signal-desktop-${SIGNAL_VERSION} || fail "Couldn't remove signal-desktop-${SIGNAL_VERSION}."
 sudo alien --to-rpm --generate signal-desktop_${SIGNAL_VERSION}_${ALIEN_TARGET}.deb || fail "Failed to create RPM source directory."
 pushd signal-desktop-${SIGNAL_VERSION} || fail "signal-desktop-${SIGNAL_VERSION} does not exist."
 sudo sed -i'' 's/^Summary: $/Summary: Signal Desktop Client/' signal-desktop-*.spec || fail "Failed to run sed on signal-desktop-*.spec."
-sudo rpmbuild --nodeps signal-desktop-*.spec || fail "Failed to run rpmbuild."
+
+sudo rpmbuild --nodeps -bb --buildroot $(pwd) signal-desktop-*.spec || fail "Failed to run rpmbuild."
+
 popd || fail "popd failed (#1)"
 popd || fail "popd failed (#2)"
-mv release/signal-desktop-${SIGNAL_VERSION}/*.rpm . || fail "Failed to move RPM file."
+popd || fail "popd failed (#3)"
+mv Signal-Desktop-${SIGNAL_VERSION}/release/*.rpm . || fail "Failed to move RPM file."
